c331e840b967bc6f3026b4800e7566c8a8d0dd1d
shuru-sagarj <sagar.jadhav@shurutech.com>
2025-10-01 15:47:27 +0530

    nit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,7 +3,6 @@ import { Link, Tabs } from 'expo-router';
 import React from 'react';
 import { Pressable } from 'react-native';
 
-import { useClientOnlyValue } from '@/components/useClientOnlyValue';
 import { useColorScheme } from '@/components/useColorScheme';
 import Colors from '@/constants/Colors';
 
@@ -22,9 +21,7 @@ export default function TabLayout() {
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        // Disable the static render of the header on web
-        // to prevent a hydration error in React Navigation v6.
-        headerShown: useClientOnlyValue(false, true),
+        headerShown: true,
       }}>
       <Tabs.Screen
         name="index"
